22610935ea47f2eba5c9ad949ebee90b99c95c46
Yvan Sraka <yvan.sraka@gmail.com>
2015-05-02 00:07:18 +0200

    autopep8 some code.

--- src/hopfieldnet/trainers.py
+++ src/hopfieldnet/trainers.py
@@ -1,20 +1,21 @@
 import numpy as np
 
+
 def hebbian_training(network, input_patterns):
     """Train a network using the Hebbian learning rule"""
     n = len(input_patterns)
-    
+
     num_neurons = network.get_weights().shape[0]
-    
+
     weights = np.zeros((num_neurons, num_neurons))
-    
+
     for i in range(num_neurons):
         for j in range(num_neurons):
-            if i == j: continue
+            if i == j:
+                continue
             for m in range(n):
                 weights[i, j] += input_patterns[m][i] * input_patterns[m][j]
-                
-    weights *= 1/float(n)
-    
+
+    weights *= 1 / float(n)
+
     network.set_weights(weights)
-    
--- tests/hopfieldnettests/net/network_operation.py
+++ tests/hopfieldnettests/net/network_operation.py
@@ -4,27 +4,29 @@ import numpy as np
 
 from hopfieldnet.net import HopfieldNetwork, InvalidNetworkInputException
 
+
 class HopfieldNetworkOperationTests(unittest.TestCase):
+
     def setUp(self):
         self.net = HopfieldNetwork(3)
-    
+
     def test_evaluate(self):
         pattern = np.array([-1, 1, -1])
         output = np.array([1, -1, 1])
-        
+
         weights = np.array([[0, 1, -1],
                             [1, 0, 0],
                             [-1, 1, 0]])
-        
+
         self.net.set_weights(weights)
-        
+
         result = self.net.evaluate(pattern)
-        
+
         self.assertTrue(np.array_equal(result, output), "The network outputs doesn't match the expected result")
 
     def test_throw_exception_on_invalid_input(self):
         input_pattern = np.ones(10)
-        
+
         self.assertRaises(InvalidNetworkInputException, self.net.evaluate, input_pattern)
 
 if __name__ == "__main__":

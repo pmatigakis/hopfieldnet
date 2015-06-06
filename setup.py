b1a42da0b9fa369cdb03d8e36fc60f0b0caea9b4
Panagiotis Matigakis <pmatigakis@gmail.com>
2015-06-06 14:48:25 +0300

    Added the setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name="hopfieldnet",
+      version="0.0.1",
+      description="Hopfield network library",
+      author="Panagiotis Matigakis",
+      author_email="pmatigakis@gmail.com",
+      packages=["hopfieldnet"],
+      package_dir = {"": "src"})
